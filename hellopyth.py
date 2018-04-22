3b52b07b6192091410076d82771db30a9d835282
Juliana <36381288+jmayy121@users.noreply.github.com>
2018-04-22 19:46:30 -0400

    code for hello world

--- hellopyth.py
+++ hellopyth.py
@@ -1,8 +1,1 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print("Hello, World")
-Hello, World
->>> 
- RESTART: C:/Users/ymayj/AppData/Local/Programs/Python/Python36/hellopyth.py 
-Hello, World
->>> 
+print("Hello, World!")
